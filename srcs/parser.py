4f5304e1fc7c63160f804e5ec62a2474d847df46
MuChinchilas <michilesjulia@gmail.com>
2026-06-22 14:55:46 +0100

    unfinished parser

--- srcs/parser.py
+++ srcs/parser.py
@@ -3,16 +3,24 @@ from errors import MapError
 from typing import Optional, Dict
 #FIXME: be consistent with returning a value or assigning directly inside method
 # as in, return nb_drones vs. self.nb_drones = nb_drones (inside method)
+# crate a Map object and store all that info there, not in Parser?
+
+# " parsing error must stop the program and return a clear error 
+# message indicating the line and cause."
+# FIXME: turn self.lines into a list of tuples where each line is paired to its
+# original number from the map file (considering empty lines/comments) so we can # print error messages with line number
 
 
 class Parser():
 
     def __init__(self, filename: str) -> None:
+        
+        #TODO: add typehints
         self.filename = filename
         self.lines = []
         self.nb_drones = None
         self.zones = {}
-        self.connections = []
+        self.connections: list[tuple] = []
 
     def _clean_map(self) -> list[str]:
         try:
@@ -52,6 +60,8 @@ class Parser():
         if '[' in body:
             clear_body, md_str = body.split('[', 1)
         parts = clear_body.split()
+        else:
+            parts = body.split()
         if len(parts) < 3:
             raise MapError(
                     f"'{hub_type}' zone missing "
@@ -68,22 +78,40 @@ class Parser():
         if '-' in name:
             raise MapError("Zone names cannot contain dashes.")
         if name in self.zones:
-            raise MapError("Found duplicate zone name: '{name}'")
+            raise MapError(f"Found duplicate zone name: '{name}'")
         
         metadata = {}
         if md_str:
-            metadata = _parse_metadata(md_str)
+            metadata = self._parse_metadata(md_str)
         
-        return hub_type, name, x, y, metadata
+        return name, {
+            "type": hub_type,
+            "x": x,
+            "y": y,
+            "metadata": metadata
+        }
 
     
-    def _parse_connection(self):
-    # Connections are defined using connection: <name1>-<name2> [metadata]:
-        #  Define a bidirectional connection (edge) between two zones.
-        #  The connection syntax forbids dashes in zone names.
-        #  Optional metadata can be specified in brackets [...]:
-        #  max_link_capacity=<number> (default: 1) - Maximum drones that
-        #    can traverse this connection simultaneously
+    def _parse_connection(self, connect_str: str) -> tuple:
+        
+        metadata = {}
+        
+        prexif, body = connect_str.split(":", 1)
+        if '[' in body:
+            clear_body, md_str = body.split('[', 1)
+        if clear_body:
+            body = clear_body
+        if '-' not in body:
+            raise MapError(
+                    "Invalid connection: "
+                    " 'connect_str', must contain '-'"
+                    )
+        connection = body.split('-')
+        
+        if md_str:
+            metadata = _parse_metadata(md_str)
+
+        return connection, metadata
     
     def _parse_metadata(self, md_str: str) -> Dict[str, str]:
         
@@ -116,11 +144,14 @@ class Parser():
             # Also strange, why would I raise the same error again?
         
         """ parse from the second line onwards """
+        # Should add a try/except here?
         for line in self.lines[1::]:
             if line.startswith(("hub:", "start_hub:", "end_hub:")):
-                zone = self._parse_zone(line) # will return a tuple
-                self.zones.update(zone)
-            elif line.startswith()
+                name, zone_data = self._parse_zone(line)
+                self.zones[name] = zone_data
+            elif line.startswith("connection:"):
+                connection = self._parse_connection(line)
+                self.connections.append(connection)
 
 
 if __name__ == "__main__":
--- to_do.md
+++ to_do.md
@@ -1,13 +1,22 @@
 # Parser:
 - Parses map file in a data structure and partially validates it so far
 [X] fixed most issues I hope...
-[ ] missing parse_connections
-[ ] parse_map is not storing data in the dict properly (see chat)
+[X] missing parse_connections
+[X] parse_map is not storing data in the dict properly (see chat)
 [ ] implement the rest of parse_map
-[ ] split it in more classes?
+[ ] HAS ERRORS! (see chat)
 [ ] Actually test everything
+[ ] Reestructure Parser:
+    [ ] Create Map object and store map data there instead of in Parser 
+    [ ] Modify Parser so that it returns a Map object
+    [ ] Create a separate Validate module to further validate Map object
+    [ ] Move some validation logic from Parser to Validate
+        (keep in parser only what is necessary for it to work)
 
 # General
 [ ] add a validate method to parser or create another
+[ ] think about project structure:
+    - What should be a package with init?
+    - How to organize directories?
 [ ] choose algorithm(s)
 [ ] decide graph's structure
